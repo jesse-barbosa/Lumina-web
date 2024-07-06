00f95c7df4d95a7c54df3086cf80167debcc8726
Jessé Barbosa <barbosajesse419@gmail.com>
2024-07-06 16:56:25 -0300

    Adding sessions and including classes

--- pages/workspaces.php
+++ pages/workspaces.php
@@ -1,229 +1,54 @@
+<?php
+require_once('../class/conexao.php');
+require_once('../class/manipularDados.php');
+
+session_start();
+
+$manipularDados = new ManipularDados();
+
+if (isset($_SESSION['email']) && isset($_SESSION['password'])) {
+    if ($manipularDados->loginUser($_SESSION['email'], $_SESSION['password'])) {
+        $username = $_SESSION['username'];
+        $email = $_SESSION['email'];
+    } else {
+        header("Location: login.php");
+        exit;
+    }
+} else {
+    header("Location: login.php");
+    exit;
+}
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="../css/styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <title>Notten</title>
+    <style>
+        main {
+        display: flex;
+        }
+    </style>
 </head>
 <body>
-<header class="header">
-    <section class="section">
-        <h1 class="Logo-inicial fw-bold fs-4">Notten</h1>
-        <nav class="navigation">
-            <h1 class="titulos fs-6 fw-semibold mt-1">Início</h1>
-            <ul class="list">
-                <a href="../index.html" class="link link-underline link-underline-opacity-0"><li><i class="bi bi-house-door me-2"></i>Página inicial</li></a>
-                <!-- Abrir o modal de pesquisa -->
-                <button class="bg-transparent border-0" data-bs-toggle="modal" data-bs-target="#searchModal"> 
-                <li><i class="bi bi-search me-2"></i>Pesquisar</li>
-                </button>
-                <!-- Abrir o modal de notificações -->
-                <button class="bg-transparent border-0" data-bs-toggle="modal" data-bs-target="#notificationModal"> 
-                <li><a href="#"><i class="bi bi-bell me-2"></i>Notificações</a></li>
-                </button>
-                <!-- Abrir o modal de configurações -->
-                <button class="bg-transparent border-0" data-bs-toggle="modal" data-bs-target="#settingsModal"> 
-                <li><a href="#"><i class="bi bi-gear me-2"></i>Configurações</a></li>
-                </button>
-            </ul>
-        <h1 class="titulos fs-6 fw-semibold mt-1">Particular</h1>
-        <ul class="list"> 
-            <a href="workspaces.html" class="link link-underline link-underline-opacity-0"><li class="li-active">Workspace 1</li></a>
-            <!-- Botão para abrir o modal de adicionar página -->
-            <button class="bg-transparent border-0 text-light" data-bs-toggle="modal" data-bs-target="#addPageModal">
-                <li><i class="bi bi-plus-circle me-2"></i>Adicionar Página</li>
-            </button>
-        </ul>
-            <h1 class="titulos fs-6 fw-semibold mt-1">Mais</h1>
-            <ul class="list">
-                <a href="https://jesse-barbosa.github.io/notten/" class="link link-underline link-underline-opacity-0" target=”_blank”><li><i class="bi bi-question-circle me-2"></i>Suporte</li></a>
-            </ul>
-        </nav>
-    </section>
-</header>
-<main>
-<!-- Modal de Pesquisa -->
-<div class="modal fade" id="searchModal" tabindex="-1" aria-labelledby="searchModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-        <div class="modal-content text-bg-dark">
-            <div class="modal-header">
-                <h5 class="modal-title" id="searchModalLabel">Pesquisar anotações</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <section>
-                    <div class="container-fluid py-1">
-                        <div class="row">
-                            <div class="col text-center">
-                                <div class="fs-5 pb-4 text-light">
-                                Busque por anotações salvas
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row justify-content-center">
-                            <div class="col">
-                                <form action="" method="get" class="form-control border-0 bg-transparent">
-                                    <div class="container">
-                                        <div class="row">
-                                            <div class="col">
-                                                <div class="input-group">
-                                                <input type="search" name="" id="" class="form-control bg-dark text-light" placeholder="Digite palavras chaves aqui">
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- Modal de Notificações -->
-<div class="modal fade" id="notificationModal" tabindex="-1" aria-labelledby="notificationModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content text-bg-dark">
-            <div class="modal-header">
-            <h5 class="modal-title" id="notificationModalLabel">Notificações</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <section>
-                    <div class="container-fluid py-1">
-                        <div class="row">
-                            <div class="col text-center">
-                                <div class="fs-5 pb-4 text-light">
-                                Veja suas notificações recentes
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row justify-content-center">
-                            <div class="col">
-                            <!-- Notificações -->
-                            <p>Não há novas notificações.</p>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- Modal de Configurações -->
-<section class="modalConfiguracoes">
-    <div class="modal fade" id="settingsModal" tabindex="-1" aria-labelledby="settingsModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered modal-lg">
-            <div class="modal-content text-bg-dark">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="settingsModalLabel">Configurações</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-3">
-                                <nav class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-                                    <a class="link-light nav-link active" id="v-pills-theme-tab" data-bs-toggle="pill" href="#v-pills-theme" role="tab" aria-controls="v-pills-theme" aria-selected="true">Aparência</a>
-                                    <a class="link-light nav-link" id="v-pills-account-tab" data-bs-toggle="pill" href="#v-pills-account" role="tab" aria-controls="v-pills-account" aria-selected="false">Conta</a>
-                                    <a class="link-light nav-link" id="v-pills-notifications-tab" data-bs-toggle="pill" href="#v-pills-notifications" role="tab" aria-controls="v-pills-notifications" aria-selected="false">Recursos</a>
-                                    <a class="link-light nav-link" id="v-pills-language-tab" data-bs-toggle="pill" href="#v-pills-language" role="tab" aria-controls="v-pills-language" aria-selected="false">Idioma</a>
-                                </nav>
-                            </div>
-                            <div class="col-9">
-                                <div class="tab-content" id="v-pills-tabContent">
-                                    <div class="tab-pane fade show active" id="v-pills-theme" role="tabpanel" aria-labelledby="v-pills-theme-tab">
-                                    <h5 class="text-light mb-4">Configurações de Aparência</h5>
-                                        <div class="mb-3 form-check">
-                                        <input type="checkbox" class="form-check-input me-3" id="lightMode">
-                                        <label class="form-check-label" for="lightMode">Modo claro</label>
-                                        </div>
-                                    </div>
-                                    <div class="tab-pane fade" id="v-pills-account" role="tabpanel" aria-labelledby="v-pills-account-tab">
-                                    <h5 class="text-light mb-4">Configurações de Conta</h5>
-                                        <div class="mb-3">
-                                            <label for="username" class="form-label">Nome de usuário</label>
-                                            <input type="text" class="form-control" id="username" placeholder="Digite seu nome de usuário">
-                                        </div>
-                                        <div class="mb-3">
-                                            <label for="email" class="form-label">Email</label>
-                                            <input type="email" class="form-control" id="email" placeholder="Digite seu email">
-                                        </div>
-                                        <div class="mb-3">
-                                            <label for="password" class="form-label">Senha</label>
-                                            <input type="password" class="form-control" id="password" placeholder="Digite sua senha">
-                                        </div>
-                                    </div>
-                                    <div class="tab-pane fade" id="v-pills-notifications" role="tabpanel" aria-labelledby="v-pills-notifications-tab">
-                                    <h5 class="text-light mb-4">Configurações de Recursos</h5>
-                                    <div class="mb-3">
-                                    <label for="notifications" class="form-label">Notificações</label>
-                                        <select class="form-select" id="notifications">
-                                            <option value="enabled">Ativado</option>
-                                            <option value="disabled">Desativado</option>
-                                        </select>
-                                    </div>
-                                    </div>
-                                    <div class="tab-pane fade" id="v-pills-language" role="tabpanel" aria-labelledby="v-pills-language-tab">
-                                        <h5 class="text-light mb-4">Configurações de Idioma</h5>
-                                        <div class="mb-3">
-                                        <label for="language" class="form-label">Idioma</label>
-                                            <select class="form-select" id="language">
-                                                <option value="pt">Português</option>
-                                                <option value="en">Inglês</option>
-                                                <option value="es">Espanhol</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+    <?php include_once("../includes/menu.php"); ?>
+    <main>
+        <?php include_once("../includes/modals.php"); ?>
+        <section class="main-principal mt-5 w-100 vh-100" id="main-principal">
+            <div class="container-fluid">
+                <div class="row justify-content-end">
+                    <div class="col-1">
+                        <?php include_once("../includes/tools.php"); ?>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
-</section>
-    
-<!-- Modal de Adicionar Página -->
-<div class="modal fade" id="addPageModal" tabindex="-1" aria-labelledby="addPageModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content text-bg-dark">
-            <div class="modal-header">
-                <h5 class="modal-title" id="addPageModalLabel">Adicionar Nova Página</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <form id="addPageForm">
-                    <div class="mb-3">
-                        <label for="pageName" class="form-label">Nome da Página</label>
-                        <input type="text" class="form-control" id="pageName" value="Nova Workspace">
-                    </div>
-                    <button type="submit" class="btn btn-outline-light">Adicionar Página</button>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
-<section class="main-principal mt-5 w-100 vh-100" id="main-principal">
-    <div class="dropdown">
-        <div class="dropdown-container">
-            <button id="add-button" class="btn btn-primary rounded-circle" onclick="toggleDropdown()">
-            <i class="bi bi-plus"></i>
-            </button>
-            <div id="dropdown" class="dropdown-menu bg-dark">
-                <button id="add-text" class="dropdown-item text-light">Adicionar Texto</button>
-                <button id="add-list" class="dropdown-item text-light">Adicionar Lista</button>
-            </div>
-        </div>
-    </div>
-</section>
-</main>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-<script src="../script.js"></script>
+        </section>
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="../js/scripts.js"></script>
 </body>
 </html>
